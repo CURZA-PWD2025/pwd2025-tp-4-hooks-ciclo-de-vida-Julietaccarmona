ffc4e7b496ff50e1a5be833d8390de8c2053d83d
Julieta <julieta_carmona_99@hotmail.com>
2025-06-02 11:50:29 -0300

    lista

---
+++ vite-project/src/components/Lista.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const elementos = ref([]);
+
+onMounted(() => {
+  setTimeout(() => {
+    // Simula datos cargados luego de 3 segundos
+    elementos.value = ["Elemento 1", "Elemento 2", "Elemento 3"];
+  }, 3000);
+});
+</script>
+
+<template>
+  <div>
+    <h2>Lista de elementos</h2>
+    <ul>
+      <li v-for="(item, index) in elementos" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: #888;
+}
+</style>
